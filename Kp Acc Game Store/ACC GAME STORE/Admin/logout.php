f99145a813e18c2ed38f5ada5ae0c81233be702e
Radennmikoo <144119316+Radennmikoo@users.noreply.github.com>
2023-11-24 17:55:42 +0700

    Delete Kp Acc Game Store directory

--- Kp Acc Game Store/ACC GAME STORE/Admin/logout.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-unset($_SESSION['admin']);
-
- echo "<script>alert('Anda Telah Logout Sampai Jumpa');</script>";
- echo "<script>location='login.php';</script>";
- ?>
--- Kp Acc Game Store/ACC GAME STORE/logout.php
+++
@@ -1,8 +0,0 @@
-<?php 
-session_start();
-
-unset($_SESSION["pelanggan"]);
-
- echo "<script>alert('Anda Telah Logout Sampai Jumpa');</script>";
- echo "<script>location='index.php';</script>";
- ?>
